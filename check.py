19cf4c26cd4c501465bbcf94e21ad5edf99a0090
Rahul Pandit <rpandit6566@gmail.com>
2025-12-23 16:47:16 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,7 +1,7 @@
 from google import genai
 
 # PASTE YOUR API KEY HERE
-API_KEY = "AIzaSyDWlCy_563-2r3xvefB7Bqw4oW9p-CQu_o"
+API_KEY = 
 
 client = genai.Client(api_key=API_KEY)
 
@@ -11,4 +11,5 @@ try:
     for m in client.models.list():
         print(f"Model found: {m.name}")
 except Exception as e:
+
     print(f"Error: {e}")
